3915806231cd59e53cf4b1a16f9a6e0a06aaf37c
Trung <vutrung26072001@gmail.com>
2024-08-30 09:20:30 +0700

    feat: install cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 
 app.get('', async (req, res) => {
 	res.status(200).json({
