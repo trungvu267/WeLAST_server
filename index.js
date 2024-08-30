c55fb1cc8e0e9396ebf0bca2794a02d4c8bc4279
Trung <vutrung26072001@gmail.com>
2024-08-30 09:19:23 +0700

    feat: implement the  API endpoint

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ app.get('', async (req, res) => {
 	});
 });
 
+app.get('/repos', async (req, res) => {
+
+    try {
+        const response = await axios.get('https://api.github.com/users/freeCodeCamp/repos');
+
+        let filtered_repos = response.data.filter(repo => !repo.fork && repo.forks > 5);
+
+		const sort_direction = req.query.sort_direction?.toUpperCase() || 'DESC';
+        filtered_repos = filtered_repos.sort((a, b) => {
+            if (sort_direction === 'ASC') {
+                return new Date(a.created_at) - new Date(b.created_at);
+            } else {
+                return new Date(b.created_at) - new Date(a.created_at);
+            }
+        });
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(filtered_repos);
+
+    } catch (error) {
+        console.error('Error fetching data from GitHub API:', error);
+        res.status(500).json({ message: 'Failed to fetch repositories from GitHub' });
+    }
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
